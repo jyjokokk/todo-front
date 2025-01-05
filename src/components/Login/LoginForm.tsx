9bdd6cca9ae722b1a8e83680d1628aa132ec99b1
Jyrki Kokkola <j.j.kokkola@gmail.com>
2025-01-05 08:54:55 +0200

    styles

--- src/components/Login/LoginForm.tsx
+++ src/components/Login/LoginForm.tsx
@@ -1,6 +1,10 @@
 import React, { useState } from 'react'
+import './LoginForm.scss'
+import { useNavigate } from 'react-router-dom'
 
 const LoginForm = () => {
+  const navigate = useNavigate()
+
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
@@ -8,27 +12,28 @@ const LoginForm = () => {
     e.preventDefault()
     console.log('credentials', { email, password })
   }
-
   return (
     <div className="component-LoginForm">
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Email"
-        />
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
-        />
-        <button type="submit" className="border-2">
-          Login
-        </button>
-      </form>
+      <div className="container">
+        <h1>Login</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+          />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+          />
+          <button type="submit" className="border-2">
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
   )
 }
